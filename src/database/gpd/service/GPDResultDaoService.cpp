57de545f4a09e736f28834264959f6f671c26022
Bryan Berthou <bryan25.berthou@laposte.net>
2015-11-06 16:07:55 +0000

    refs#16
    - Improved transaction and callback for database ; Correcting errors

--- src/database/gpd/service/GPDResultDaoService.cpp
+++ src/database/gpd/service/GPDResultDaoService.cpp
@@ -4,33 +4,55 @@
 #include <exception>
 #include <vector>
 
-#include "../../beans/gpd/GPDResult.h"
-#include "../../beans/gpd/GPDResultList.h"
-#include "../../beans/parton_distribution/PartonDistribution.h"
-#include "../dao/GPDResultDao.h"
-#include "GPDKinematicDaoService.h"
-#include "ModuleDaoService.h"
-#include "PartonDistributionDaoService.h"
-#include "QuarkDistributionDaoService.h"
+#include "../../../beans/gpd/GPDResult.h"
+#include "../../../beans/gpd/GPDResultList.h"
+#include "../../../beans/parton_distribution/PartonDistribution.h"
+#include "../../service/ModuleDaoService.h"
+#include "../../service/PartonDistributionDaoService.h"
+#include "../../service/QuarkDistributionDaoService.h"
 
 GPDResultDaoService::GPDResultDaoService() :
         BaseObject("GPDResultDaoService") {
-
 }
 
 GPDResultDaoService::~GPDResultDaoService() {
 
 }
 
-int GPDResultDaoService::insert(const GPDResult &gpdResult) {
+int GPDResultDaoService::insert(const GPDResult &gpdResult) const {
+
+    int result = -1;
+
+    // For multiple query it's better to use transaction to guarantee database's integrity and performance
+    QSqlDatabase::database().transaction();
+
+    try {
+
+        result = insertWithoutTransaction(gpdResult);
+
+        // If there is no exception we can commit all query
+        QSqlDatabase::database().commit();
+
+    } catch (std::exception &e) {
+        error(__func__, e.what());
+
+        // Else return database in a stable state : n-1
+        QSqlDatabase::database().rollback();
+    }
+
+    return result;
+}
+
+int GPDResultDaoService::insertWithoutTransaction(
+        const GPDResult &gpdResult) const {
 
     // Check if this gpd_kinematic already exists
-    int gpdKinematicId = GPDKinematicDaoService::select(
+    int gpdKinematicId = m_gpdKinematicDaoService.select(
             gpdResult.getKinematic());
 
     // If not, insert new entry in database and retrieve its id
     if (gpdKinematicId == -1) {
-        gpdKinematicId = GPDKinematicDaoService::insert(
+        gpdKinematicId = m_gpdKinematicDaoService.insert(
                 gpdResult.getKinematic());
     }
 
@@ -39,7 +61,7 @@ int GPDResultDaoService::insert(const GPDResult &gpdResult) {
             gpdResult.getComputedByGpdModuleId());
 
     // Insert new gpd_result entry in database
-    int gpdResultId = GPDResultDao::insert(moduleId, gpdKinematicId);
+    int gpdResultId = m_gpdResultDao.insert(moduleId, gpdKinematicId);
 
     std::vector<PartonDistribution> partonDistributionList =
             gpdResult.getPartonDistributionList();
@@ -57,7 +79,7 @@ int GPDResultDaoService::insert(const GPDResult &gpdResult) {
     return gpdResultId;
 }
 
-int GPDResultDaoService::insert(const GPDResultList &gpdResultList) {
+int GPDResultDaoService::insert(const GPDResultList &gpdResultList) const {
     int result = -1;
 
     // For multiple query it's better to use transaction to guarantee database's integrity and performance
@@ -66,7 +88,7 @@ int GPDResultDaoService::insert(const GPDResultList &gpdResultList) {
     try {
 
         for (unsigned int i = 0; i != gpdResultList.getSize(); i++) {
-            result = insert(gpdResultList.get(i));
+            result = insertWithoutTransaction(gpdResultList.get(i));
         }
 
         // If there is no exception we can commit all query
---
+++ src/database/gpd/service/GPDResultDaoService.h
@@ -0,0 +1,26 @@
+#ifndef GPD_RESULT_DAO_SERVICE
+#define GPD_RESULT_DAO_SERVICE
+
+#include "../../dao/GPDResultDao.h"
+#include "GPDKinematicDaoService.h"
+
+class GPDResult;
+class GPDResultList;
+
+class GPDResultDaoService: public BaseObject {
+public:
+    GPDResultDaoService();
+    virtual ~ GPDResultDaoService();
+
+    int insert(const GPDResult &gpdResult) const;
+    int insert(const GPDResultList &gpdResultList) const;
+
+private:
+    GPDResultDao m_gpdResultDao;
+
+    GPDKinematicDaoService m_gpdKinematicDaoService;
+
+    int insertWithoutTransaction(const GPDResult &gpdResult) const;
+};
+
+#endif /* GPD_RESULT_DAO_SERVICE */
