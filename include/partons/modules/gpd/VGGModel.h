ff5e1f3fab6b8571fce9ae13898d9ad938b36f8e
Pawel Sznajder <pawel.sznajder@ncbj.gov.pl>
2015-12-16 15:45:13 +0000

    refs#16 - Initial version

---
+++ include/partons/modules/gpd/VGGModel.h
@@ -0,0 +1,85 @@
+/*
+ * VGGModel.h
+ *
+ *  Created on: Dec 10, 2015
+ *      Author: Michel Guidal (IPNO) and Pawel Sznajder (IPNO)
+ */
+
+#ifndef VGGMODEL_H_
+#define VGGMODEL_H_
+
+#include "../GPDModule.h"
+#include <NumA/MathIntegrator.h>
+
+class c_mstwpdf;
+
+class VGGModel: public GPDModule {
+
+public:
+
+    static const unsigned int classId;
+
+    VGGModel(const std::string &className);
+
+    virtual ~VGGModel();
+
+    virtual VGGModel* clone() const;
+
+    virtual void init();
+
+    virtual void configure(ParameterList parameters);
+
+    virtual std::string toString();
+
+protected:
+
+    VGGModel(const VGGModel& other);
+
+    virtual void isModuleWellConfigured();
+    virtual void initModule();
+
+    virtual PartonDistribution computeH();
+    virtual PartonDistribution computeE();
+
+private:
+
+    enum flavour{undefined, upv, upsea, dnv, dnsea}; //< flavour
+
+    static const double eps_doubleint; //< step to skip x = 0 singularity
+    static const double kappa_u; //< anomalous magnetic moment of u quarks
+    static const double kappa_d; //< anomalous magnetic moment of d quarks
+
+    double b_profile_val; //< b slope for valence quarks
+    double b_profile_sea; //< b slope for sea quarks
+    double alphap_val; //< Ragge parameter for valence quarks
+    double alphap_sea; //< Ragge parameter for sea quarks
+    double eta_e_largex_u_s; //< Shape parameter for GPD E^uVal
+    double eta_e_largex_d_s; //< Shape parameter for GPD E^dVal
+
+    c_mstwpdf* m_Forward;   //< pdfs
+
+    NumA::MathIntegrator* m_mathIntegrator; //< integration routine
+    GPDType gpd_s5;
+    flavour flavour_s5; //< flavour - internal variables used in integration
+    double x_s5; //< x - internal variables used in integration
+
+    double offforward_distr(); //< double distribution scheme
+    double symm_profile_function(double beta, double alpha, double b_profile); //< profile function
+
+    double symm_double_distr_reggeH(double beta, double alpha); //< double distribution function for GPD H
+    double int_symm_double_distr_reggeH(double* alpha, double* par); //< wrapper for integration of double distribution at x for GPD H
+    double int_symm_double_distr_reggeMxH(double* alpha, double* par); //< wrapper for integration of double distribution at -x for GPD H
+
+    double symm_double_distr_reggeE(double beta, double alpha); //< double distribution function for GPD E
+    double int_symm_double_distr_reggeE(double* alpha, double* par); //< wrapper for integration of double distribution at x for GPD E
+    double int_symm_double_distr_reggeMxE(double* alpha, double* par); //< wrapper for integration of double distribution at -x for GPD E
+    double int_mom2_up_valence_e(double* x, double* par);   //< normalization function for GPD E
+
+    double test_pdf_down_val(double x); //< pdfs for cross-check purpose, parametrization of Martin, Roberts, Stirling, PRD 47 (1993) 867
+    double test_pdf_up_val(double x);
+    double test_pdf_sea(double x);
+    double test_pdf_up_bar(double x);
+    double test_pdf_down_bar(double x);
+};
+
+#endif /* VGGMODEL_H_ */
---
+++ src/partons/modules/gpd/VGGModel.cpp
@@ -0,0 +1,592 @@
+/*
+ * VGGModel.cpp
+ *
+ *  Created on: Dec 10, 2015
+ *      Author: Michel Guidal (IPNO) and Pawel Sznajder (IPNO)
+ */
+
+#include "../../../../include/partons/modules/gpd/VGGModel.h"
+
+#include <stdio.h>
+#include <math.h>
+#include <stdexcept>
+
+#include "../../../../include/partons/utils/mstwpdf.h"
+#include "../../../../include/partons/BaseObjectRegistry.h"
+#include "../../../../include/partons/utils/PropertiesManager.h"
+#include "../../../../include/partons/utils/stringUtils/Formatter.h"
+
+const unsigned int VGGModel::classId =
+        BaseObjectRegistry::getInstance()->registerBaseObject(
+                new VGGModel("VGGModel"));
+
+const double VGGModel::eps_doubleint = 0.001;
+const double VGGModel::kappa_u = 1.6596;
+const double VGGModel::kappa_d = -2.0352;
+
+VGGModel::VGGModel(const std::string &className) :
+        GPDModule(className) {
+
+    b_profile_val = -1.;
+    b_profile_sea = -1.;
+    alphap_val = -1.;
+    alphap_sea = -1.;
+    eta_e_largex_u_s = -1.;
+    eta_e_largex_d_s = -1.;
+
+    m_mathIntegrator = 0;
+    m_Forward = 0;
+
+    gpd_s5 = GPDType::UNDEFINED;
+    flavour_s5 = undefined;
+    x_s5 = -1.;
+
+    m_listGPDComputeTypeAvailable.insert(
+            std::make_pair(GPDType::H, &GPDModule::computeH));
+
+    m_listGPDComputeTypeAvailable.insert(
+            std::make_pair(GPDType::E, &GPDModule::computeE));
+}
+
+VGGModel::~VGGModel() {
+
+    if (m_mathIntegrator) {
+        delete m_mathIntegrator;
+        m_mathIntegrator = 0;
+    }
+    if (m_Forward) {
+        delete m_Forward;
+        m_Forward = 0;
+    }
+}
+
+VGGModel* VGGModel::clone() const {
+    return new VGGModel(*this);
+}
+
+void VGGModel::init() {
+
+    //TODO are the same for GPDs H and E?
+    b_profile_val = 1.;
+    b_profile_sea = 1.;
+    alphap_val = 1.105;
+    alphap_sea = 1.105;
+    eta_e_largex_u_s = 1.713;
+    eta_e_largex_d_s = 0.566;
+
+    m_mathIntegrator = new NumA::MathIntegrator();
+    m_mathIntegrator->setIntegrationMode(NumA::MathIntegrator::ROOT);
+    m_Forward = new c_mstwpdf(
+            PropertiesManager::getInstance()->getString("grid.directory")
+                    + "mstw2008nlo.00.dat");
+}
+
+void VGGModel::configure(ParameterList parameters) {
+}
+
+std::string VGGModel::toString() {
+    return GPDModule::toString();
+}
+
+VGGModel::VGGModel(const VGGModel& other) :
+        GPDModule(other) {
+
+    b_profile_val = other.b_profile_val;
+    b_profile_sea = other.b_profile_sea;
+    alphap_val = other.alphap_val;
+    alphap_sea = other.alphap_sea;
+    eta_e_largex_u_s = other.eta_e_largex_u_s;
+    eta_e_largex_d_s = other.eta_e_largex_d_s;
+
+    m_mathIntegrator = other.m_mathIntegrator;
+    m_Forward = other.m_Forward;
+
+    gpd_s5 = other.gpd_s5;
+    flavour_s5 = other.flavour_s5;
+    x_s5 = other.x_s5;
+}
+
+void VGGModel::isModuleWellConfigured() {
+    GPDModule::isModuleWellConfigured();
+
+    if (b_profile_val == -1.)
+        throw std::runtime_error("[VGGModel] Unknown b_profile_val");
+
+    if (b_profile_sea == -1.)
+        throw std::runtime_error("[VGGModel] Unknown b_profile_sea");
+
+    if (alphap_val == -1.)
+        throw std::runtime_error("[VGGModel] Unknown alphap_val");
+
+    if (alphap_sea == -1.)
+        throw std::runtime_error("[VGGModel] Unknown alphap_sea");
+
+    if (eta_e_largex_u_s == -1.)
+        throw std::runtime_error("[VGGModel] Unknown eta_e_largex_u_s");
+
+    if (eta_e_largex_d_s == -1.)
+        throw std::runtime_error("[VGGModel] Unknown eta_e_largex_d_s");
+
+    if (m_mathIntegrator == NULL)
+        throw std::runtime_error("[VGGModel] MathIntegration module is NULL");
+
+    if (m_mathIntegrator->getIntegrationMode()
+            == NumA::MathIntegrator::UNDEFINED)
+        throw std::runtime_error("[VGGModel] MathIntegrationMode is UNDEFINED");
+
+    if (m_Forward == NULL)
+        throw std::runtime_error("[VGGModel] PDF module is NULL");
+}
+
+void VGGModel::initModule() {
+
+    GPDModule::initModule();
+}
+
+PartonDistribution VGGModel::computeH() {
+
+    //GPD
+    gpd_s5 = GPDType::H;
+
+    //variables
+    double uVal, uSea, dVal, dSea, sSea, g;
+    double uValMx, dValMx;
+
+    //GPDs for x
+    x_s5 = m_x;
+
+    flavour_s5 = upv;
+    uVal = offforward_distr();
+
+    flavour_s5 = dnv;
+    dVal = offforward_distr();
+
+    flavour_s5 = upsea;
+    uSea = offforward_distr();
+
+    flavour_s5 = dnsea;
+    dSea = offforward_distr();
+
+    sSea = 0.;
+    g = 0.;
+
+    //GPDs for -x
+    x_s5 = -m_x;
+
+    flavour_s5 = upv;
+    uValMx = offforward_distr();
+
+    flavour_s5 = dnv;
+    dValMx = offforward_distr();
+
+    //store results
+    QuarkDistribution quarkDistribution_u(QuarkFlavor::UP);
+    QuarkDistribution quarkDistribution_d(QuarkFlavor::DOWN);
+    QuarkDistribution quarkDistribution_s(QuarkFlavor::STRANGE);
+
+    quarkDistribution_u.setQuarkDistribution(uVal + uSea);
+    quarkDistribution_d.setQuarkDistribution(dVal + dSea);
+    quarkDistribution_s.setQuarkDistribution(sSea);
+
+    quarkDistribution_u.setQuarkDistributionPlus(uVal - uValMx + 2 * uSea);
+    quarkDistribution_d.setQuarkDistributionPlus(dVal - dValMx + 2 * dSea);
+    quarkDistribution_s.setQuarkDistributionPlus(2 * sSea);
+
+    quarkDistribution_u.setQuarkDistributionMinus(uVal + uValMx);
+    quarkDistribution_d.setQuarkDistributionMinus(dVal + dValMx);
+    quarkDistribution_s.setQuarkDistributionMinus(0.);
+
+    GluonDistribution gluonDistribution(g);
+
+    PartonDistribution partonDistribution;
+
+    partonDistribution.setGluonDistribution(gluonDistribution);
+    partonDistribution.addQuarkDistribution(quarkDistribution_u);
+    partonDistribution.addQuarkDistribution(quarkDistribution_d);
+    partonDistribution.addQuarkDistribution(quarkDistribution_s);
+
+    return partonDistribution;
+}
+
+PartonDistribution VGGModel::computeE() {
+
+    //GPD
+    gpd_s5 = GPDType::E;
+
+    //variables
+    double uVal, uSea, dVal, dSea, sSea, g;
+    double uValMx, dValMx;
+
+    //GPDs for x
+    x_s5 = m_x;
+
+    flavour_s5 = upv;
+    uVal = kappa_u * offforward_distr()
+            / m_mathIntegrator->integrateWithROOT(this,
+                    &VGGModel::int_mom2_up_valence_e, 0., 1.);
+
+    flavour_s5 = dnv;
+    dVal = kappa_d * offforward_distr()
+            / m_mathIntegrator->integrateWithROOT(this,
+                    &VGGModel::int_mom2_up_valence_e, 0., 1.);
+
+    uSea = 0.;
+    dSea = 0.;
+    sSea = 0.;
+    g = 0.;
+
+    //GPDs for -x
+    x_s5 = -m_x;
+
+    flavour_s5 = upv;
+    uValMx = offforward_distr();
+
+    flavour_s5 = dnv;
+    dValMx = offforward_distr();
+
+    //store results
+    QuarkDistribution quarkDistribution_u(QuarkFlavor::UP);
+    QuarkDistribution quarkDistribution_d(QuarkFlavor::DOWN);
+    QuarkDistribution quarkDistribution_s(QuarkFlavor::STRANGE);
+
+    quarkDistribution_u.setQuarkDistribution(uVal + uSea);
+    quarkDistribution_d.setQuarkDistribution(dVal + dSea);
+    quarkDistribution_s.setQuarkDistribution(sSea);
+
+    quarkDistribution_u.setQuarkDistributionPlus(uVal - uValMx + 2 * uSea);
+    quarkDistribution_d.setQuarkDistributionPlus(dVal - dValMx + 2 * dSea);
+    quarkDistribution_s.setQuarkDistributionPlus(2 * sSea);
+
+    quarkDistribution_u.setQuarkDistributionMinus(uVal + uValMx);
+    quarkDistribution_d.setQuarkDistributionMinus(dVal + dValMx);
+    quarkDistribution_s.setQuarkDistributionMinus(0.);
+
+    GluonDistribution gluonDistribution(g);
+
+    PartonDistribution partonDistribution;
+
+    partonDistribution.setGluonDistribution(gluonDistribution);
+    partonDistribution.addQuarkDistribution(quarkDistribution_u);
+    partonDistribution.addQuarkDistribution(quarkDistribution_d);
+    partonDistribution.addQuarkDistribution(quarkDistribution_s);
+
+    return partonDistribution;
+}
+
+double VGGModel::offforward_distr() {
+
+    //result
+    double ofpd;
+
+    //integrated function according to variant
+    double (VGGModel::*f_dist)(double* a, double* b) = NULL;
+    double (VGGModel::*f_distMx)(double* a, double* b) = NULL;
+
+    //GPD
+    switch (gpd_s5) {
+
+    case GPDType::H: {
+
+        f_dist = &VGGModel::int_symm_double_distr_reggeH;
+        f_distMx = &VGGModel::int_symm_double_distr_reggeMxH;
+
+    }
+        break;
+
+    case GPDType::E: {
+
+        f_dist = &VGGModel::int_symm_double_distr_reggeE;
+        f_distMx = &VGGModel::int_symm_double_distr_reggeMxE;
+
+    }
+        break;
+
+    default: {
+
+        throw std::runtime_error(
+                Formatter() << "[VGGModel::offforward_distr()] GPD = " << gpd_s5
+                        << " not defined");
+    }
+        break;
+    }
+
+    //three ranges of x
+    if (x_s5 >= m_xi) {
+
+        ofpd = m_mathIntegrator->integrateWithROOT(this, f_dist,
+                -(1. - x_s5) / (1. + m_xi), (1. - x_s5) / (1. - m_xi));
+
+    } else if ((-m_xi < x_s5) && (x_s5 < m_xi)) {
+
+        ofpd = m_mathIntegrator->integrateWithROOT(this, f_dist,
+                -(1. - x_s5) / (1. + m_xi), x_s5 / m_xi - eps_doubleint);
+
+        if (flavour_s5 != upv && flavour_s5 != dnv) {
+            ofpd -= m_mathIntegrator->integrateWithROOT(this, f_distMx,
+                    -(1. + x_s5) / (1. + m_xi), -x_s5 / m_xi - eps_doubleint);
+        }
+
+    } else {
+        if (flavour_s5 != upv && flavour_s5 != dnv) {
+            ofpd = -m_mathIntegrator->integrateWithROOT(this, f_distMx,
+                    -(1. + x_s5) / (1. + m_xi), (1. + x_s5) / (1. - m_xi));
+        } else {
+            ofpd = 0.;
+        }
+    }
+
+    return ofpd;
+}
+
+double VGGModel::symm_double_distr_reggeH(double beta, double alpha) {
+
+    //check beta range
+    if (beta <= 0.) {
+        throw std::runtime_error(
+                Formatter() << "[VGGModel::symm_double_distr_reggeH()] alpha = "
+                        << alpha << ", beta = " << beta
+                        << ", argument 0 or negative");
+    }
+
+    //update and get pdf
+    m_Forward->update(beta, sqrt(m_MuF2));
+
+    double pdf = -1.;
+    double b_profile = -1.;
+    double funcbetat = -1.;
+
+    switch (flavour_s5) {
+
+    case upv: {
+
+        pdf = m_Forward->cont.upv / beta;
+        //pdf = test_pdf_up_val(beta);
+        b_profile = b_profile_val;
+        funcbetat = pow(1. / fabs(beta), (1. - beta) * alphap_val * m_t);
+    }
+        break;
+
+    case dnv: {
+
+        pdf = m_Forward->cont.dnv / beta;
+        //pdf = test_pdf_down_val(beta);
+        b_profile = b_profile_val;
+        funcbetat = pow(1. / fabs(beta), (1. - beta) * alphap_val * m_t);
+    }
+        break;
+
+    case upsea: {
+
+        pdf = m_Forward->cont.usea / beta;
+        //pdf = test_pdf_up_bar(beta);
+        b_profile = b_profile_sea;
+        funcbetat = pow(1. / fabs(beta), (1. - beta) * alphap_sea * m_t);
+    }
+        break;
+
+    case dnsea: {
+
+        pdf = m_Forward->cont.dsea / beta;
+        //pdf = test_pdf_down_bar(beta);
+        b_profile = b_profile_sea;
+        funcbetat = pow(1. / fabs(beta), (1. - beta) * alphap_sea * m_t);
+    }
+        break;
+
+    default: {
+
+        throw std::runtime_error(
+                Formatter()
+                        << "[VGGModel::symm_double_distr_reggeH()] Flavour = "
+                        << flavour_s5 << " not defined");
+    }
+        break;
+
+    }
+
+    return symm_profile_function(beta, alpha, b_profile) * pdf * funcbetat;
+}
+
+double VGGModel::symm_double_distr_reggeE(double beta, double alpha) {
+
+    //check beta range
+    if (beta <= 0.) {
+        throw std::runtime_error(
+                Formatter() << "[VGGModel::symm_double_distr_reggeE()] alpha = "
+                        << alpha << ", beta = " << beta
+                        << ", argument 0 or negative");
+    }
+
+    //update and get pdf
+    m_Forward->update(beta, sqrt(m_MuF2));
+
+    double pdf = -1.;
+    double b_profile = -1.;
+    double funcbetat = -1.;
+
+    switch (flavour_s5) {
+
+    case upv: {
+
+        pdf = m_Forward->cont.upv / beta;
+        //pdf = test_pdf_up_val(beta);
+        b_profile = b_profile_val;
+        funcbetat = pow(1. - beta, eta_e_largex_u_s)
+                * pow(1. / fabs(beta), (1. - beta) * alphap_val * m_t);
+    }
+        break;
+
+    case dnv: {
+
+        pdf = m_Forward->cont.dnv / beta;
+        //pdf = test_pdf_down_val(beta);
+        b_profile = b_profile_val;
+        funcbetat = pow(1. - beta, eta_e_largex_d_s)
+                * pow(1. / fabs(beta), (1. - beta) * alphap_val * m_t);
+    }
+        break;
+
+    default: {
+
+        throw std::runtime_error(
+                Formatter()
+                        << "[VGGModel::symm_double_distr_reggeE()] Flavour = "
+                        << flavour_s5 << " not defined");
+    }
+        break;
+
+    }
+
+    return symm_profile_function(beta, alpha, b_profile) * pdf * funcbetat;
+}
+
+double VGGModel::symm_profile_function(double beta, double alpha,
+        double b_profile) {
+
+    double norm, bidon;
+
+    norm = 1. / pow(2., 2. * b_profile + 1.) * tgamma(2. * b_profile + 2.)
+            / pow(tgamma(b_profile + 1.), 2.);
+
+    bidon = pow(1. - fabs(beta), 2.) - pow(alpha, 2.);
+    if (bidon < 0.)
+        bidon = 0.;
+
+    return norm * pow(bidon, b_profile)
+            / pow(1. - fabs(beta), 2. * b_profile + 1.);
+}
+
+double VGGModel::int_symm_double_distr_reggeH(double* alpha, double* par) {
+    return symm_double_distr_reggeH(x_s5 - m_xi * (*alpha), *alpha);
+}
+
+double VGGModel::int_symm_double_distr_reggeMxH(double* alpha, double* par) {
+    return symm_double_distr_reggeH(-x_s5 - m_xi * (*alpha), *alpha);
+}
+
+double VGGModel::int_symm_double_distr_reggeE(double* alpha, double* par) {
+    return symm_double_distr_reggeE(x_s5 - m_xi * (*alpha), *alpha);
+}
+
+double VGGModel::int_symm_double_distr_reggeMxE(double* alpha, double* par) {
+    return symm_double_distr_reggeE(-x_s5 - m_xi * (*alpha), *alpha);
+}
+
+double VGGModel::int_mom2_up_valence_e(double* x, double* par) {
+
+    //value
+    double beta = *x;
+
+    //check beta range
+    if (beta <= 0.) {
+        throw std::runtime_error(
+                Formatter() << "[VGGModel::int_mom2_up_valence_e()] x = "
+                        << beta << ", argument 0 or negative");
+    }
+
+    //update and get pdf
+    m_Forward->update(beta, sqrt(m_MuF2));
+
+    double pdf = -1.;
+    double eta_e_largex_s = -1.;
+
+    switch (flavour_s5) {
+
+    case upv: {
+
+        pdf = m_Forward->cont.upv / beta;
+        //pdf = test_pdf_up_val(beta);
+        eta_e_largex_s = eta_e_largex_u_s;
+    }
+        break;
+
+    case dnv: {
+
+        pdf = m_Forward->cont.dnv / beta;
+        //pdf = test_pdf_down_val(beta);
+        eta_e_largex_s = eta_e_largex_d_s;
+
+    }
+        break;
+
+    default: {
+
+        throw std::runtime_error(
+                Formatter() << "[VGGModel::int_mom2_up_valence_e()] Flavour = "
+                        << flavour_s5 << " not defined");
+    }
+        break;
+
+    }
+
+    return pow(1. - beta, eta_e_largex_s) * pdf;
+}
+
+double VGGModel::test_pdf_down_val(double x) {
+
+    double n3, n4, ed, gd, norm;
+
+    n3 = 0.78;
+    n4 = 4.57;
+    ed = -0.87;
+    gd = 0.82;
+    norm = 0.263275;
+
+    return 1. / norm * 1. / pow(x, 1. - n3) * pow(1. - x, n4)
+            * (1. + ed * pow(x, 0.5) + gd * x);
+}
+
+double VGGModel::test_pdf_up_val(double x) {
+
+    double n1, n2, eud, gud, norm;
+
+    n1 = 0.26;
+    n2 = 3.82;
+    eud = 14.4;
+    gud = 16.99;
+    norm = 9.790758;
+
+    return 3. / norm * 1. / pow(x, 1. - n1) * pow(1. - x, n2)
+            * (1. + eud * pow(x, 0.5) + gud * x) - test_pdf_down_val(x);
+}
+
+double VGGModel::test_pdf_sea(double x) {
+
+    double ns, es, gs, As;
+
+    ns = 10.;
+    es = -2.21;
+    gs = 6.22;
+    As = 1.87;
+
+    return As * 1. / x * pow(1. - x, ns) * (1. + es * pow(x, 0.5) + gs * x);
+}
+
+double VGGModel::test_pdf_up_bar(double x) {
+    return 1. / 5. * test_pdf_sea(x);
+}
+
+double VGGModel::test_pdf_down_bar(double x) {
+    return 1. / 5. * test_pdf_sea(x);
+}
+
