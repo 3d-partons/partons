a3c2685e1146157c2ea4a47cfb396301e3c8af06
Pawel Sznajder <pawel.sznajder@ncbj.gov.pl>
2016-09-27 12:27:32 +0000

    refs#16
    In PARTONS:
    - DVCSConstantCFFModel: refactoring and adding the possibility to set CFF separately for each CFF

--- include/partons/modules/convol_coeff_function/DVCS/DVCSConstantCFFModel.h
+++ include/partons/modules/convol_coeff_function/DVCS/DVCSConstantCFFModel.h
@@ -13,32 +13,57 @@
  */
 
 #include <complex>
+#include <map>
 #include <string>
-#include <vector>
+//#include <vector>
 
+#include "../../../beans/gpd/GPDType.h"
 #include "DVCSConvolCoeffFunctionModule.h"
 
+/**
+ * Module to set CFF values via automation or via configure() function
+ */
 class DVCSConstantCFFModel: public DVCSConvolCoeffFunctionModule {
+
 public:
 
     static const unsigned int classId; ///< Unique ID to automatically register the class in the registry.
 
-    static const std::string CFF_VALUES;
+    static const std::string CFF_VALUES; ///< Key name to set several CFFs via automation
 
+    static const std::string CFF_H_Re; ///< Key name to set real part of CFF H via automation
+    static const std::string CFF_H_Im; ///< Key name to set imaginary part of CFF H via automation
+    static const std::string CFF_E_Re; ///< Key name to set real part of CFF E via automation
+    static const std::string CFF_E_Im; ///< Key name to set imaginary part of CFF E via automation
+    static const std::string CFF_Ht_Re; ///< Key name to set real part of CFF Ht via automation
+    static const std::string CFF_Ht_Im; ///< Key name to set imaginary part of CFF Ht via automation
+    static const std::string CFF_Et_Re; ///< Key name to set real part of CFF Et via automation
+    static const std::string CFF_Et_Im; ///< Key name to set imaginary part of CFF Et via automation
+
+    /**
+     * Default constructor
+     @param className Name of class
+     */
     DVCSConstantCFFModel(const std::string &className);
 
+    /**
+     * Clone
+     */
     virtual DVCSConstantCFFModel* clone() const;
 
+    /**
+     * Destructor
+     */
     virtual ~DVCSConstantCFFModel();
 
     virtual void configure(const ElemUtils::Parameters &parameters);
-
     virtual std::complex<double> computeCFF();
 
 protected:
+
     /**
      * Copy constructor
-     * @param other
+     @param other Object to be copied
      */
     DVCSConstantCFFModel(const DVCSConstantCFFModel &other);
 
@@ -46,12 +71,20 @@ protected:
     virtual void isModuleWellConfigured();
 
 private:
+
+    /**
+     * Set single value in configure
+     @param gpdType CFF type to be set
+     @param isRealPart True if real part to be set
+     @param value Value to be set
+     */
+    void configureValue(GPDType::Type gpdType, bool isRealPart, const ElemUtils::GenericType& value);
+
     /**
      * CFFs stored by type of GPD
-     * [2]=H, [3]=E, [4]=Ht, etc.
      * See GPDType.h
      */
-    std::vector<std::complex<double> > m_CFF;
+    std::map<GPDType::Type, std::complex<double> > m_CFF;
 };
 
 #endif /* DVCS_CONSTANT_CFF_MODULE_H */
--- src/partons/modules/convol_coeff_function/DVCS/DVCSConstantCFFModel.cpp
+++ src/partons/modules/convol_coeff_function/DVCS/DVCSConstantCFFModel.cpp
@@ -4,79 +4,47 @@
 #include <ElementaryUtils/parameters/Parameters.h>
 #include <ElementaryUtils/string_utils/Formatter.h>
 #include <ElementaryUtils/string_utils/StringUtils.h>
-#include <map>
 #include <utility>
+#include <vector>
 
-#include "../../../../../include/partons/beans/gpd/GPDType.h"
 #include "../../../../../include/partons/BaseObjectRegistry.h"
 
-// Initialise [class]::classId with a unique name.
+// initialize [class]::classId with a unique name.
 const unsigned int DVCSConstantCFFModel::classId =
         BaseObjectRegistry::getInstance()->registerBaseObject(
                 new DVCSConstantCFFModel("DVCSConstantCFFModel"));
 
+// set key names
 const std::string DVCSConstantCFFModel::CFF_VALUES = "cff_values";
+
+const std::string DVCSConstantCFFModel::CFF_H_Re = "cff_value_H_Re";
+const std::string DVCSConstantCFFModel::CFF_H_Im = "cff_value_H_Im";
+const std::string DVCSConstantCFFModel::CFF_E_Re = "cff_value_E_Re";
+const std::string DVCSConstantCFFModel::CFF_E_Im = "cff_value_E_Im";
+const std::string DVCSConstantCFFModel::CFF_Ht_Re = "cff_value_Ht_Re";
+const std::string DVCSConstantCFFModel::CFF_Ht_Im = "cff_value_Ht_Im";
+const std::string DVCSConstantCFFModel::CFF_Et_Re = "cff_value_Et_Re";
+const std::string DVCSConstantCFFModel::CFF_Et_Im = "cff_value_Et_Im";
 
 DVCSConstantCFFModel::DVCSConstantCFFModel(const std::string &className) :
         DVCSConvolCoeffFunctionModule(className) {
     setIsGPDModuleDependent(false);
 
-    m_CFF.assign(GPDType::END, 0.);
+    //initialize
+    m_CFF.clear();
 
+    //insert and assign
     for (int i = static_cast<int>(GPDType::H);
             i < static_cast<int>(GPDType::END); i++) {
+
+        //insert to map
+        m_CFF.insert(std::make_pair(static_cast<GPDType::Type>(i), std::complex<double>(0., 0.)));
+
+        //assign function
         m_listOfCFFComputeFunctionAvailable.insert(
                 std::make_pair(static_cast<GPDType::Type>(i),
                         &DVCSConvolCoeffFunctionModule::computeCFF));
     }
-//    m_listOfCFFComputeFunctionAvailable.insert(
-//            std::make_pair(GPDType::H,
-//                    &DVCSConstantCFFModel::computeCFF));
-//    m_listOfCFFComputeFunctionAvailable.insert(
-//            std::make_pair(GPDType::E,
-//                    &DVCSConstantCFFModel::computeCFF));
-//    m_listOfCFFComputeFunctionAvailable.insert(
-//            std::make_pair(GPDType::Ht,
-//                    &DVCSConstantCFFModel::computeCFF));
-//    m_listOfCFFComputeFunctionAvailable.insert(
-//            std::make_pair(GPDType::Et,
-//                    &DVCSConstantCFFModel::computeCFF));
-//    m_listOfCFFComputeFunctionAvailable.insert(
-//            std::make_pair(GPDType::HTrans,
-//                    &DVCSConstantCFFModel::computeCFF));
-//    m_listOfCFFComputeFunctionAvailable.insert(
-//            std::make_pair(GPDType::ETrans,
-//                    &DVCSConstantCFFModel::computeCFF));
-//    m_listOfCFFComputeFunctionAvailable.insert(
-//            std::make_pair(GPDType::HtTrans,
-//                    &DVCSConstantCFFModel::computeCFF));
-//    m_listOfCFFComputeFunctionAvailable.insert(
-//            std::make_pair(GPDType::EtTrans,
-//                    &DVCSConstantCFFModel::computeCFF));
-//    m_listOfCFFComputeFunctionAvailable.insert(
-//            std::make_pair(GPDType::H3p,
-//                    &DVCSConstantCFFModel::computeCFF));
-//    m_listOfCFFComputeFunctionAvailable.insert(
-//            std::make_pair(GPDType::E3p,
-//                    &DVCSConstantCFFModel::computeCFF));
-//    m_listOfCFFComputeFunctionAvailable.insert(
-//            std::make_pair(GPDType::Ht3p,
-//                    &DVCSConstantCFFModel::computeCFF));
-//    m_listOfCFFComputeFunctionAvailable.insert(
-//            std::make_pair(GPDType::Et3p,
-//                    &DVCSConstantCFFModel::computeCFF));
-//    m_listOfCFFComputeFunctionAvailable.insert(
-//            std::make_pair(GPDType::H3m,
-//                    &DVCSConstantCFFModel::computeCFF));
-//    m_listOfCFFComputeFunctionAvailable.insert(
-//            std::make_pair(GPDType::E3m,
-//                    &DVCSConstantCFFModel::computeCFF));
-//    m_listOfCFFComputeFunctionAvailable.insert(
-//            std::make_pair(GPDType::Ht3m,
-//                    &DVCSConstantCFFModel::computeCFF));
-//    m_listOfCFFComputeFunctionAvailable.insert(
-//            std::make_pair(GPDType::Et3m,
-//                    &DVCSConstantCFFModel::computeCFF));
 }
 
 DVCSConstantCFFModel::DVCSConstantCFFModel(const DVCSConstantCFFModel &other) :
@@ -93,51 +61,170 @@ DVCSConstantCFFModel::~DVCSConstantCFFModel() {
 }
 
 void DVCSConstantCFFModel::initModule() {
-    // init parent module before
+
+    // initialize parent module before
     DVCSConvolCoeffFunctionModule::initModule();
 }
 
 void DVCSConstantCFFModel::isModuleWellConfigured() {
+
     // check parent module before
     DVCSConvolCoeffFunctionModule::isModuleWellConfigured();
 }
 
 std::complex<double> DVCSConstantCFFModel::computeCFF() {
-    return m_CFF[m_currentGPDComputeType];
+
+    std::map<GPDType::Type, std::complex<double> >::iterator it = m_CFF.find(
+            m_currentGPDComputeType);
+
+    if (it == m_CFF.end()) {
+
+        warn(__func__,
+                ElemUtils::Formatter() << "CFF of type "
+                        << GPDType(m_currentGPDComputeType).toString()
+                        << " is not supported by this class");
+
+        return std::complex<double>(0., 0.);
+
+    } else {
+        return it->second;
+    }
 }
 
 void DVCSConstantCFFModel::configure(const ElemUtils::Parameters &parameters) {
+
+    //clear
+    for (std::map<GPDType::Type, std::complex<double> >::iterator it =
+            m_CFF.begin(); it != m_CFF.end(); it++) {
+        (it->second) = std::complex<double>(0., 0.);
+    }
+
+    // several CFFs
     if (parameters.isAvailable(DVCSConstantCFFModel::CFF_VALUES)) {
 
+        // to string
         std::string temp_str = parameters.getLastAvailable().toString();
+
+        // check if not empty
         if (!temp_str.empty()) {
+
+            // split string
             std::vector<std::string> CFFValues = ElemUtils::StringUtils::split(
                     temp_str, '|');
 
-            for (int i = 0;
-                    i < CFFValues.size()
-                            and i
-                                    < 2
-                                            * (static_cast<int>(GPDType::END)
-                                                    - static_cast<int>(GPDType::H));
-                    i = i + 2) {
-                int j = static_cast<int>(GPDType::H) + i / 2;
-                m_CFF[j] =
-                        std::complex<double>(
-                                ElemUtils::GenericType(CFFValues[i]).toDouble(),
-                                i + 1 < CFFValues.size() ?
-                                        ElemUtils::GenericType(CFFValues[i + 1]).toDouble() :
-                                        0.);
+            // iterator
+            std::map<GPDType::Type, std::complex<double> >::iterator it =
+                    m_CFF.begin();
 
-                info(__func__,
-                        ElemUtils::Formatter() << "CFF of type "
-                                << GPDType(static_cast<GPDType::Type>(j)).toString()
-                                << " configured with value = "
-                                << m_CFF[j].real() << " + i*"
-                                << m_CFF[j].imag());
+            // assign
+            for (int i = 0; i < CFFValues.size(); i++) {
+
+                //check if not end
+                if (it == m_CFF.end()) {
+
+                    warn(__func__,
+                            ElemUtils::Formatter()
+                                    << "Too many elements for key "
+                                    << DVCSConstantCFFModel::CFF_VALUES
+                                    << ", additional elements will be ignored");
+                    break;
+                }
+
+                //assign and increment
+                if (i % 2 == 0) {
+
+                    (it->second).real(
+                            ElemUtils::GenericType(CFFValues.at(i)).toDouble());
+
+                    info(__func__,
+                            ElemUtils::Formatter() << "Real part of CFF type "
+                                    << GPDType(it->first).toString()
+                                    << " configured with value = "
+                                    << (it->second).real());
+                } else {
+
+                    (it->second).imag(
+                            ElemUtils::GenericType(CFFValues.at(i)).toDouble());
+
+                    info(__func__,
+                            ElemUtils::Formatter()
+                                    << "Imaginary part of CFF type "
+                                    << GPDType(it->first).toString()
+                                    << " configured with value = "
+                                    << (it->second).imag());
+
+                    it++;
+                }
             }
+
+        } else {
+            warn(__func__,
+                    ElemUtils::Formatter() << "String for "
+                            << DVCSConstantCFFModel::CFF_VALUES << " is empty");
         }
     }
 
+    // single CFFs
+    if (parameters.isAvailable(DVCSConstantCFFModel::CFF_H_Re)) {
+        configureValue(GPDType::H, 1, parameters.getLastAvailable());
+    }
+    if (parameters.isAvailable(DVCSConstantCFFModel::CFF_H_Im)) {
+        configureValue(GPDType::H, 0, parameters.getLastAvailable());
+    }
+    if (parameters.isAvailable(DVCSConstantCFFModel::CFF_E_Re)) {
+        configureValue(GPDType::E, 1, parameters.getLastAvailable());
+    }
+    if (parameters.isAvailable(DVCSConstantCFFModel::CFF_E_Im)) {
+        configureValue(GPDType::E, 0, parameters.getLastAvailable());
+    }
+    if (parameters.isAvailable(DVCSConstantCFFModel::CFF_Ht_Re)) {
+        configureValue(GPDType::Ht, 1, parameters.getLastAvailable());
+    }
+    if (parameters.isAvailable(DVCSConstantCFFModel::CFF_Ht_Im)) {
+        configureValue(GPDType::Ht, 0, parameters.getLastAvailable());
+    }
+    if (parameters.isAvailable(DVCSConstantCFFModel::CFF_Et_Re)) {
+        configureValue(GPDType::Et, 1, parameters.getLastAvailable());
+    }
+    if (parameters.isAvailable(DVCSConstantCFFModel::CFF_Et_Im)) {
+        configureValue(GPDType::Et, 0, parameters.getLastAvailable());
+    }
+
+    //configure mother class
     DVCSConvolCoeffFunctionModule::configure(parameters);
+}
+
+void DVCSConstantCFFModel::configureValue(GPDType::Type gpdType,
+        bool isRealPart, const ElemUtils::GenericType& value) {
+
+    //iterator
+    std::map<GPDType::Type, std::complex<double> >::iterator it = m_CFF.find(
+            gpdType);
+
+    //check if not end
+    if (it == m_CFF.end()) {
+
+        warn(__func__,
+                ElemUtils::Formatter() << "CFF of type "
+                        << GPDType(gpdType).toString()
+                        << " is not supported by this class");
+    }
+
+    //value
+    double valueDouble = value.toDouble();
+
+    //set
+    if (isRealPart) {
+        (it->second).real(valueDouble);
+    } else {
+        (it->second).imag(valueDouble);
+    }
+
+    //status
+    info(__func__,
+            ElemUtils::Formatter() << ((isRealPart) ?
+                    ("Real") :
+                    ("Imaginary")) << " part of CFF type "
+                            << GPDType(gpdType).toString()
+                            << " configured with value = " << valueDouble);
 }
