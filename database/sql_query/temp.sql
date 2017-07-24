5de5047a074ddfadcd8d1a567bce83e8e05b4d3a
Pawel Sznajder <pawel.sznajder@ncbj.gov.pl>
2017-07-24 06:07:40 -0400

    remove unused files #28

--- database/sql_query/temp.sql
+++
@@ -1,9 +0,0 @@
-SELECT DISTINCT obr.id, obr.observable_name , obr.observable_value , obr.phi
-FROM observable_result obr, observable_kinematic obk
-WHERE obr.observable_kinematic_id = obk.id
-AND obk.id = '1'
-AND obr.computation_id = '2';
-
-SELECT *
-FROM observable_kinematic
-WHERE id = '1';
