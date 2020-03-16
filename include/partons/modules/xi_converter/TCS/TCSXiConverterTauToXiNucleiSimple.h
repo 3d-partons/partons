c8ef34da3c4a46d430e72e10ef419db3a6a96c55
Pawel Sznajder <pawel.sznajder@ncbj.gov.pl>
2020-03-16 09:03:10 +0100

    add xi converter module

---
+++ include/partons/modules/xi_converter/TCS/TCSXiConverterTauToXiNucleiSimple.h
@@ -0,0 +1,59 @@
+#ifndef TCS_XI_CONVERTER_TAU_TO_XI_NUCLEI_SIMPLE_H
+#define TCS_XI_CONVERTER_TAU_TO_XI_NUCLEI_SIMPLE_H
+
+/**
+ * @file TCSXiConverterTauToXiNucleiSimple.h
+ * @author Bryan BERTHOU (SPhN / CEA Saclay)
+ * @date 28 October 2015
+ * @version 1.0
+ */
+
+#include <string>
+
+#include "../../../utils/type/PhysicalType.h"
+#include "TCSXiConverterModule.h"
+
+namespace PARTONS {
+
+/**
+ * @class TCSXiConverterTauToXiNucleiSimple
+ *
+ * @brief Evaluation of GPD variable xi from \f$\tau\f$ for TCS.
+ *
+ * \f$\tau = Q'^{2}/(s-M^{2})\f$
+ * \f$\xi = \tau/(2 - \tau)\f$
+ */
+class TCSXiConverterTauToXiNucleiSimple: public TCSXiConverterModule {
+
+public:
+
+    /**
+     * Unique ID to automatically register the class in the registry.
+     */
+    static const unsigned int classId;
+
+    /**
+     * Constructor.
+     * @param className Name of class.
+     */
+    TCSXiConverterTauToXiNucleiSimple(const std::string &className);
+
+    /**
+     * Copy constructor.
+     * @param other Object to be copied.
+     */
+    TCSXiConverterTauToXiNucleiSimple(const TCSXiConverterTauToXiNucleiSimple &other);
+
+    /**
+     * Destructor.
+     */
+    virtual ~TCSXiConverterTauToXiNucleiSimple();
+
+    virtual TCSXiConverterTauToXiNucleiSimple* clone() const;
+    virtual PhysicalType<double> compute(
+            const TCSObservableKinematic& kinematic);
+};
+
+} /* namespace PARTONS */
+
+#endif /* TCS_XI_CONVERTER_TAU_TO_XI_NUCLEI_SIMPLE_H */
---
+++ src/partons/modules/xi_converter/TCS/TCSXiConverterTauToXiNucleiSimple.cpp
@@ -0,0 +1,47 @@
+#include "../../../../../include/partons/modules/xi_converter/TCS/TCSXiConverterTauToXiNucleiSimple.h"
+
+#include <cmath>
+
+#include "../../../../../include/partons/beans/observable/TCS/TCSObservableKinematic.h"
+#include "../../../../../include/partons/BaseObjectRegistry.h"
+#include "../../../../../include/partons/FundamentalPhysicalConstants.h"
+#include "../../../../../include/partons/utils/type/PhysicalUnit.h"
+
+namespace PARTONS {
+
+const unsigned int TCSXiConverterTauToXiNucleiSimple::classId =
+        BaseObjectRegistry::getInstance()->registerBaseObject(
+                new TCSXiConverterTauToXiNucleiSimple("TCSXiConverterTauToXiNucleiSimple"));
+
+TCSXiConverterTauToXiNucleiSimple::TCSXiConverterTauToXiNucleiSimple(const std::string &className) :
+        TCSXiConverterModule(className) {
+}
+
+TCSXiConverterTauToXiNucleiSimple::TCSXiConverterTauToXiNucleiSimple(const TCSXiConverterTauToXiNucleiSimple &other) :
+        TCSXiConverterModule(other) {
+}
+
+TCSXiConverterTauToXiNucleiSimple::~TCSXiConverterTauToXiNucleiSimple() {
+}
+
+TCSXiConverterTauToXiNucleiSimple* TCSXiConverterTauToXiNucleiSimple::clone() const {
+    return new TCSXiConverterTauToXiNucleiSimple(*this);
+}
+
+PhysicalType<double> TCSXiConverterTauToXiNucleiSimple::compute(
+        const TCSObservableKinematic& kinematic) {
+
+    double s =
+            pow(Constant::PROTON_MASS, 2)
+                    + 2. * Constant::PROTON_MASS
+                            * kinematic.getE().makeSameUnitAs(PhysicalUnit::GEV).getValue();
+
+    double Q2Prim =
+            kinematic.getQ2Prim().makeSameUnitAs(PhysicalUnit::GEV2).getValue();
+
+    double tau = Q2Prim / (s - pow(Constant::PROTON_MASS, 2));
+
+    return PhysicalType<double>(tau / (2. - tau), PhysicalUnit::NONE);
+}
+
+} /* namespace PARTONS */
